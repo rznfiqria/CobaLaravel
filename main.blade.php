d401da291b3e1e142c672c2c953a96592f160b0c
rznfiqria <rznfiqria@gmail.com>
2019-10-18 00:19:54 +0700

    push ke github versi2

--- main.blade.php
+++ main.blade.php
@@ -2,7 +2,8 @@
     <head>
         <title>Belajar push ke github via VScode</title>
         <body>
-            <h1> selamat datang</h1>
+            <h1>selamat datang</h1>
+            <p>ini cuma untuk pembelajaran push ke github saja men.</p>
         </body>
     </head>
 </html>
